a5c9cf8ea7e23b0da7cf40cc3b069718ec8400d1
Jero Sanchez <jero.sanchez@icloud.com>
2020-09-12 19:25:15 +0200

    Extract serialization details to a helper method

--- Tests/BusMADCoreTests/Nearest Stops API/LoadNearestStopsFromRemoteUseCaseTests.swift
+++ Tests/BusMADCoreTests/Nearest Stops API/LoadNearestStopsFromRemoteUseCaseTests.swift
@@ -77,8 +77,8 @@ class LoadNearestStopsFromRemoteUseCaseTests: XCTestCase {
         let stop2 = makeStop(id: 2)
         
         expect(sut, toCompleteWith: .success([stop1.model, stop2.model]), when: {
-            let stopsJSON = ["data": [stop1.json, stop2.json]]
-            client.complete(withStatusCode: 200, data: try! JSONSerialization.data(withJSONObject: stopsJSON, options: []))
+            let stopsJSON = [stop1.json, stop2.json]
+            client.complete(withStatusCode: 200, data: makeJSON(stopsJSON))
         })
     }
     
@@ -122,6 +122,11 @@ class LoadNearestStopsFromRemoteUseCaseTests: XCTestCase {
         ]
 
         return (stop, stopJSON)
+    }
+    
+    private func makeJSON(_ stops: [[String: Any]]) -> Data {
+        let json = ["data": stops]
+        return try! JSONSerialization.data(withJSONObject: json, options: [])
     }
         
     private func emptyJSON() -> Data {
