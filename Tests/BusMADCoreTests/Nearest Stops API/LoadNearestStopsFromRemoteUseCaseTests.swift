4ec8ab7f15cbd15f460064c46a9b95ceb725c29b
Jero Sanchez <jero.sanchez@icloud.com>
2020-09-12 16:29:53 +0200

    Load command requests data from URL

--- Tests/BusMADCoreTests/Nearest Stops API/LoadNearestStopsFromRemoteUseCaseTests.swift
+++ Tests/BusMADCoreTests/Nearest Stops API/LoadNearestStopsFromRemoteUseCaseTests.swift
@@ -6,22 +6,50 @@ import XCTest
 
 class HTTPClient {
     var requestedURLs = [URL]()
+    
+    func get(from url: URL) {
+        requestedURLs.append(url)
+    }
 }
 
 class RemoteStopsLoader {
+    let url: URL
+    let client: HTTPClient
     
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
 }
 
 class LoadNearestStopsFromRemoteUseCaseTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClient()
-        let _ = RemoteStopsLoader()
+        let _ = RemoteStopsLoader(url: url, client: client)
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
+        let client = HTTPClient()
+        let sut = RemoteStopsLoader(url: url, client: client)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs.first, url)
+    }
+    
+    // MARK: - Linux compatibility
+    
     static var allTests = [
         ("test_init_doesNotRequestDataFromURL", test_init_doesNotRequestDataFromURL),
+        ("test_load_requestsDataFromURL", test_load_requestsDataFromURL),
     ]
 }
