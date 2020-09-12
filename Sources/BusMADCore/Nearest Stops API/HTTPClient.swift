9764434857e24b6328f2989ca7d44e63293905d3
Jero Sanchez <jero.sanchez@icloud.com>
2020-09-12 17:57:05 +0200

    Extract `HTTPClient` type to its own file

---
+++ Sources/BusMADCore/Nearest Stops API/HTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Jero Sánchez on 12/09/2020.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
