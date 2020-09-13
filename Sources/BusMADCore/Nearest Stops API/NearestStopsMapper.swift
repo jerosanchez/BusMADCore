b5dd25197a8558a099f4a223adec414cbdb3775e
Jero Sanchez <jero.sanchez@icloud.com>
2020-09-13 19:30:59 +0200

    Extract magic numbers to private static vars

--- Sources/BusMADCore/Nearest Stops API/NearestStopsMapper.swift
+++ Sources/BusMADCore/Nearest Stops API/NearestStopsMapper.swift
@@ -10,14 +10,18 @@ internal class NearestStopsMapper {
         internal let description: String
         internal let data: [RemoteNearestStop]?
     }
+    
+    private static var OK_200: Int { return 200 }
+    
+    private static var EXPIRED_SESSION_ERROR: String { return "80" }
 
     internal static func map(_ data: Data, with response: HTTPURLResponse) -> RemoteNearestStopsLoader.Result {
-        if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
+        if response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) {
             
             if let stops = (root.data?.map { $0.model }) {
                 return .success(stops)
             } else {
-                if root.code == "80" {
+                if root.code == EXPIRED_SESSION_ERROR {
                     return .failure(RemoteNearestStopsLoader.Error.expiredSession)
                 } else {
                     return .failure(RemoteNearestStopsLoader.Error.invalidRequest)
