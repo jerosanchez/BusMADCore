1f02f73733694e6f04302dbdea15c5e8b425405a
Jero Sanchez <jero.sanchez@icloud.com>
2020-09-19 18:06:27 +0200

    Make `URLSessionHTTPClient` conform to `HTTPClient` protocol

---
+++ Sources/BusMADCore/Nearest Stops API/URLSessionHTTPClient.swift
@@ -0,0 +1,27 @@
+//
+//  Created by Jero Sánchez on 19/09/2020.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    public struct UnexpectedValuesRepresentation: Error { }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
--- Tests/BusMADCoreTests/Nearest Stops API/URLSessionHTTPClientTests.swift
+++ Tests/BusMADCoreTests/Nearest Stops API/URLSessionHTTPClientTests.swift
@@ -70,7 +70,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     // MARK: Helpers
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> URLSessionHTTPClient {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
