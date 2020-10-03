9c6ea688048961df75d8683046715fb07265f32b
Jero Sanchez <jero.sanchez@icloud.com>
2020-10-03 14:17:11 +0200

    Extract test logic to a helper method to improve readability

--- Tests/EndToEndTests/AccessTokenAPIEndToEndTests.swift
+++ Tests/EndToEndTests/AccessTokenAPIEndToEndTests.swift
@@ -8,26 +8,32 @@ import BusMADCore
 class AccessTokenAPIEndToEndTests: XCTestCase {
     
     func test_EndToEndGETAccessTokenResult_receivesAValidAccessToken() {
+        switch getAccessTokenResult() {
+        case .success:
+            break
+        case let .failure(error):
+            XCTFail("Expected successfull load result, got \(error) instead.")
+        default:
+            XCTFail("Expected successfull result, got no result instead.")
+        }
+    }
+    
+    // MARK: Helpers
+    
+    private func getAccessTokenResult(file: StaticString = #file, line: UInt = #line) -> LoadAccessTokenResult? {
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let serviceURL = URL(string: "https://openapi.emtmadrid.es/v2/mobilitylabs/user/login/")!
         let loader = RemoteAccessTokenLoader(from: serviceURL, client: client)
 
         let exp = expectation(description: "Wait for load completion")
         
-        var receivedToken: AccessToken?
+        var receivedResult: LoadAccessTokenResult?
         loader.load(clientId: CLIENT_ID, passKey: PASS_KEY) { result in
-            switch result {
-            case let .success(accessToken):
-                receivedToken = accessToken
-            case let .failure(error):
-                XCTFail("Expected an access token, got error \(error) instead.")
-            }
-            
+            receivedResult = result
             exp.fulfill()
         }
         
         wait(for: [exp], timeout: 5.0)
-        
-        XCTAssertNotNil(receivedToken)
+        return receivedResult
     }
 }
