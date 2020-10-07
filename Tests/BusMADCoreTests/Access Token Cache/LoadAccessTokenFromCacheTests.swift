7f86441024bafcac1c8cf89d3264b0fd21f69bb0
Jero Sanchez <jero.sanchez@icloud.com>
2020-10-07 19:24:11 +0200

    Load command in `LocalAccessTokenLoader` fails on retrieval error

--- Tests/BusMADCoreTests/Access Token Cache/LoadAccessTokenFromCacheTests.swift
+++ Tests/BusMADCoreTests/Access Token Cache/LoadAccessTokenFromCacheTests.swift
@@ -5,8 +5,12 @@
 import XCTest
 import BusMADCore
 
+enum RetrieveCachedAccessTokenResult {
+    case failure(Error)
+}
+
 class AccessTokenStore {
-    typealias RetrieveCompletion = (AccessToken) -> Void
+    typealias RetrieveCompletion = (RetrieveCachedAccessTokenResult) -> Void
     
     private var retrieveCompletions = [RetrieveCompletion]()
     
@@ -20,6 +24,10 @@ class AccessTokenStore {
         retrieveCompletions.append(completion)
         receivedMessages.append(.retrieve)
     }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrieveCompletions[index](.failure(error))
+    }
 }
 
 class LocalAccessTokenLoader: AccessTokenLoader {
@@ -30,7 +38,12 @@ class LocalAccessTokenLoader: AccessTokenLoader {
     }
     
     func load(clientId: String, passKey: String, completion: @escaping (LoadAccessTokenResult) -> Void) {
-        store.retrieve() { _ in }
+        store.retrieve() { result in
+            switch result {
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
     }
 }
 
@@ -44,10 +57,19 @@ class LoadAccessTokenFromCacheTests: XCTestCase {
     
     func test_load_requestsCacheRetrieval() {
         let (sut, store) = makeSUT()
-        
+        
         sut.load(clientId: "a client", passKey: "a pass key") { _ in }
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failsOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        let retrievalError = anyNSError()
+        
+        expect(sut, toCompleteWith: .failure(retrievalError), when: {
+            store.completeRetrieval(with: retrievalError)
+        })
     }
     
     // MARK: - Helpers
@@ -56,6 +78,29 @@ class LoadAccessTokenFromCacheTests: XCTestCase {
         let store = AccessTokenStore()
         let sut = LocalAccessTokenLoader(store: store)
         return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 1)
+    }
+    
+    private func expect(_ sut: LocalAccessTokenLoader, toCompleteWith expectedResult: LoadAccessTokenResult, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load(clientId: "a client", passKey: "a pass key") { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(expectedFeed, receivedFeed, file: file, line: line)
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(expectedError, receivedError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        action()
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK: - Linux compatibility
